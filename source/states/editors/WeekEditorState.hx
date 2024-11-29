c14c1f054accc17d035333d9b8ef57d511a62688
Hadrian767-2 <hadriannfs976@gmail.com>
2024-11-28 22:04:48 -0300

    Initial commit

--- source/states/editors/WeekEditorState.hx
+++ source/states/editors/WeekEditorState.hx
@@ -502,16 +502,11 @@ class WeekEditorState extends MusicBeatState implements PsychUIEventHandler.Psyc
 		var data:String = haxe.Json.stringify(weekFile, "\t");
 		if (data.length > 0)
 		{
-			#if mobile
-			unsavedProgress = false;
-			StorageUtil.saveContent('$weekFileName.json', data);
-			#else
 			_file = new FileReference();
 			_file.addEventListener(#if desktop Event.SELECT #else Event.COMPLETE #end, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
 			_file.save(data, weekFileName + ".json");
-			#end
 		}
 	}
 	
